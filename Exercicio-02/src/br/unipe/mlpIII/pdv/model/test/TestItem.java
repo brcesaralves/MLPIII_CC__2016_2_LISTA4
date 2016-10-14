c99cfb80caf370f067863b6ba26760f34c26b721
promotor <promotor@note-desur-01>
2016-10-14 09:56:08 -0300

    Implementação da classe de teste testItem

--- Exercicio-02/src/br/unipe/mlpIII/pdv/model/test/TestItem.java
+++ Exercicio-02/src/br/unipe/mlpIII/pdv/model/test/TestItem.java
@@ -1,5 +1,17 @@
 package br.unipe.mlpIII.pdv.model.test;
 
-public class TestItem {
+import br.unipe.mlpIII.pdv.model.Item;
+import junit.framework.TestCase;
 
+public class TestItem extends TestCase{
+	
+	public void testCompareTo(){
+		
+		Item item1 = new Item(123456, "Monitor Beng 21 IPS", 799.90);
+		Item item2 = new Item(555678, "HD 1TB Seagate", 299.90);
+		
+		assertEquals(item1.compareTo(item2), -1);
+		
+	}
+	
 }
